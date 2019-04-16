8424c62c069293c47ab136c30b4b8b0b8c9eac26
Seregfaroth <bl@tokni.com>
2019-04-16 13:49:27 +0100

    add buttons to leftcontainer

---
+++ src/MainContainer/ContentContainer/MainContent/LeftContainer/LeftContainer.style.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  width: 300px;
+  flex-direction: column;
+`;
+
+export const LeftMenuItem = styled.div`
+  @import url("https://fonts.googleapis.com/css?family=Montserrat:400");
+  font-family: "Montserrat", sans-serif;
+  font-size: 16px;
+  height: 40px;
+  width: 200px;
+  color: white;
+  background: #2b2a2a;
+  font-size: 16px;
+  font-weight: light;
+  text-align: left;
+  border-width: 0;
+  padding: 0px 0px 0px 16px;
+  cursor: pointer;
+  transition: color 300ms ease-in;
+  margin: 2px;
+  display: flex;
+  align-items: center;
+
+  :hover {
+    color: grey;
+  }
+`
--- src/MainContainer/ContentContainer/MainContent/MainContent.js
+++ src/MainContainer/ContentContainer/MainContent/MainContent.js
@@ -1,4 +1,10 @@
 import React from "react";
 import { Container } from "./MainContent.style";
+import { LeftContainer } from "./LeftContainer/LeftContainer";
 
-export const MainContent = () => <Container>Main Container</Container>;
+export const MainContent = () => (
+  <Container>
+    Main Container
+    <LeftContainer />
+  </Container>
+);
