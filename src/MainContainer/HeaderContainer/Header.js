13b9b902b8459816ff3f4696eabcfdf0303f714e
Marita <maritafholm@gmail.com>
2019-04-16 14:19:52 +0200

    created header

--- src/MainContainer/HeaderContainer/Header.js
+++ src/MainContainer/HeaderContainer/Header.js
@@ -1,4 +1,26 @@
 import React from "react";
-import { Container } from "./Header.style";
+import {
+  Container,
+  TitleContainer,
+  LogoContainer,
+  HeaderMenu,
+  HeaderMenuItem,
+  TextContainer
+} from "./Header.style";
+import logo from "./REEEMlogo.transparent.1.svg";
 
-export const Header = () => <Container>Header</Container>;
+export const Header = () => (
+  <Container>
+    <TitleContainer>
+      <LogoContainer>
+        <img src={logo} alt="logo" width={200} height={40} />
+      </LogoContainer>
+      <TextContainer>Game</TextContainer>
+    </TitleContainer>
+    <HeaderMenu>
+      <HeaderMenuItem>About</HeaderMenuItem>
+      <HeaderMenuItem>Score</HeaderMenuItem>
+      <HeaderMenuItem>Share</HeaderMenuItem>
+    </HeaderMenu>
+  </Container>
+);
