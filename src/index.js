b9c08fc9d947be3f04a7a7c1dcd05ea2cc8119cb
Olavur Ellefsen <oe@tokni.com>
2019-04-16 17:56:10 +0200

    Add Apollo connection to Hasura/Postgres

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
 import ContextStore from "./Context/ContextStore";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import client from "./reeemApolloClient";
 
 ReactDOM.render(
-  <ContextStore>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ContextStore>,
+  <ApolloProvider client={client}>
+    <ContextStore>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ContextStore>
+  </ApolloProvider>,
   document.getElementById("root")
 );
