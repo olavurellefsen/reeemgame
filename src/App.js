32508f59e902e2c450c2b893afd07a0116230840
Olavur Ellefsen <oe@tokni.com>
2019-04-18 23:21:42 +0100

    Add Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 import { MainContainer } from './MainContainer/MainContainer'
+import ReactGA from 'react-ga'
+ReactGA.initialize('UA-86120823-4', {
+  testMode: process.env.NODE_ENV === 'test',
+})
+ReactGA.pageview(window.location.pathname + window.location.search)
 
 class App extends Component {
   render() {
