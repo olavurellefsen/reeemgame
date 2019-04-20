46f6574064cb8837fd4d5db4a1e6bde692987759
Olavur Ellefsen <oe@tokni.com>
2019-04-20 14:14:48 +0100

    Change to game.reeem.org

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <ContextStore>
-    <BrowserRouter basename={'/reeemgame'}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </ContextStore>,
