598950df20214845a636dd3e353751b885958a86
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-20 15:10:49 +0000

    Deploy ReeemProject/reeemgame to github.com/ReeemProject/reeemgame.git:gh-pages

---
+++ precache-manifest.e463543c402b24fb83e031adc03345ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc50b12999e7b66a3606",
+    "url": "/static/js/main.cf665fff.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ad9aab6d1bdb00144ddc",
+    "url": "/static/js/2.9bdb7cd9.chunk.js"
+  },
+  {
+    "revision": "0eda79b90226c310bc5b7b3dd039c2ab",
+    "url": "/static/media/REEEMlogo.transparent.1.0eda79b9.svg"
+  },
+  {
+    "revision": "25c96bea518bf7e18e7abb461a6945d5",
+    "url": "/static/media/europe.25c96bea.svg"
+  },
+  {
+    "revision": "20f97690e14b67caf5e72b7048dd6a24",
+    "url": "/index.html"
+  }
+];
