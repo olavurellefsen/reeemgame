1a415b6914521e7a88ab324188a974985124cf3b
Olavur Ellefsen <oe@tokni.com>
2019-04-21 13:25:51 +0100

    Dynamic coloring of countries

--- src/MainContainer/ContentContainer/MainContent/MapContainer/MapContainer.style.js
+++ src/MainContainer/ContentContainer/MainContent/MapContainer/MapContainer.style.js
@@ -3,11 +3,22 @@ import styled from 'styled-components'
 export const Container = styled.div`
   max-width: 100%;
 `
+const sampleColors = [
+  { code: 'se', color: 'green' },
+  { code: 'dk', color: 'blue' },
+  { code: 'de', color: 'salmon' },
+  { code: 'fo', color: 'red' },
+  { code: 'uk', color: 'lightblue' },
+  { code: 'hr', color: 'orange' },
+  { code: 'be', color: 'yellow' },
+  { code: 'fi', color: 'lightgreen' },
+  { code: 'lt', color: 'lime' },
+]
+const countryColorsCSS = countryColors =>
+  countryColors.map(country => `#${country.code} {fill ${country.color}}`)
 
 export const StyledEurope = styled.div`
-  .ru {
-    fill: #aaaaaa;
-  }
+  ${countryColorsCSS(sampleColors)}
   fill: #cccccc;
   stroke: gray;
   stroke-width: 10;
