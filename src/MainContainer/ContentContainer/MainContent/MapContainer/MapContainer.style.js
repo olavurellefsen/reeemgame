5582c2ff8027b42820a6e05683d520b1826093e6
Olavur Ellefsen <oe@tokni.com>
2019-04-21 22:52:42 +0100

    Sample electricity demand BE, AT, BG

--- src/MainContainer/ContentContainer/MainContent/MapContainer/MapContainer.style.js
+++ src/MainContainer/ContentContainer/MainContent/MapContainer/MapContainer.style.js
@@ -3,48 +3,19 @@ import styled from 'styled-components'
 export const Container = styled.div`
   max-width: 100%;
 `
-const sampleColors = [
-  { code: 'se', color: 'lightblue' },
-  { code: 'dk', color: 'lightblue' },
-  { code: 'de', color: 'lightblue' },
-  { code: 'at', color: 'lightblue' },
-  { code: 'ie', color: 'lightblue' },
-  { code: 'uk', color: 'lightblue' },
-  { code: 'hr', color: 'lightblue' },
-  { code: 'nl', color: 'lightblue' },
-  { code: 'be', color: 'lightblue' },
-  { code: 'lu', color: 'lightblue' },
-  { code: 'fi', color: 'lightblue' },
-  { code: 'ee', color: 'lightblue' },
-  { code: 'lv', color: 'lightblue' },
-  { code: 'lt', color: 'lightblue' },
-  { code: 'fr', color: 'lightblue' },
-  { code: 'es', color: 'lightblue' },
-  { code: 'pt', color: 'lightblue' },
-  { code: 'it', color: 'lightblue' },
-  { code: 'mt', color: 'lightblue' },
-  { code: 'gr', color: 'lightblue' },
-  { code: 'si', color: 'lightblue' },
-  { code: 'pl', color: 'lightblue' },
-  { code: 'hu', color: 'lightblue' },
-  { code: 'cz', color: 'lightblue' },
-  { code: 'sk', color: 'lightblue' },
-  { code: 'ro', color: 'lightblue' },
-  { code: 'bg', color: 'lightblue' },
-  { code: 'cy', color: 'lightblue' },
-]
+
 const countryColorsCSS = countryColors =>
   countryColors.map(
     country => `
     #${country.code} {
       fill ${country.color};
-      :hover {fill: blue;}
+      :hover {fill: #80b3c3;}
     }
     `
   )
 
 export const StyledEurope = styled.div`
-  ${countryColorsCSS(sampleColors)}
+  ${props => countryColorsCSS(props.colors)}
   fill: #cccccc;
   stroke: gray;
   stroke-width: 10;
