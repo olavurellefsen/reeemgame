8721b0651b7ce2a7eb4efdb5f4727e6d551d955b
Olavur Ellefsen <oe@tokni.com>
2019-04-22 19:33:19 +0100

    Enable choice of year

--- src/MainContainer/ContentContainer/MainContent/TimelineContainer/TimelineContainer.js
+++ src/MainContainer/ContentContainer/MainContent/TimelineContainer/TimelineContainer.js
@@ -3,10 +3,30 @@ import ProgressBar from 'react-bootstrap/ProgressBar'
 import Context from '../../../../Context/Context'
 import { Container, Label, LabelContainer } from './TimelineContainer.style'
 
-export const TimelineContainer = () => {
-  const [state] = useContext(Context)
+const calculateCurrentYear = (event, minYear, maxYear, now) => {
+  const rect = event.target.getBoundingClientRect()
+  const newPoint = event.clientX
+  const leftPoint = rect.x
+  if (event.target.id === 'beforenow') {
+    // between minimum year and the currently selected year
+    return Math.floor(
+      minYear + ((now - minYear) * (newPoint - leftPoint)) / rect.width
+    )
+  }
+  if (event.target.id === 'afternow') {
+    return Math.round(
+      now + ((maxYear - now) * (newPoint - leftPoint)) / rect.width
+    )
+  }
 
-  const now = parseInt(state.currentDecision)
+  return now
+}
+
+export const TimelineContainer = () => {
+  const [state, dispatch] = useContext(Context)
+
+  const now = state.currentYear
+  const maxYear = state.maxYear
   const years = state.timeline
   var prevYear = 2010
   const progressInstance = (
@@ -26,7 +46,36 @@ export const TimelineContainer = () => {
           }
         })}
       </LabelContainer>
-      <ProgressBar now={now} label={`${now}`} min={2015} max={2050} />
+      <ProgressBar>
+        <ProgressBar
+          id="beforenow"
+          key={1}
+          now={now}
+          label={`${now}`}
+          min={2015}
+          max={2050}
+          onClick={event => {
+            dispatch({
+              type: 'setCurrentYear',
+              year: calculateCurrentYear(event, 2015, now, now),
+            })
+          }}
+        />
+        <ProgressBar
+          id="afternow"
+          key={2}
+          variant="warning"
+          now={maxYear - (now - 2015)}
+          min={2015}
+          max={2050}
+          onClick={event => {
+            dispatch({
+              type: 'setCurrentYear',
+              year: calculateCurrentYear(event, now, maxYear, now),
+            })
+          }}
+        />
+      </ProgressBar>
     </Container>
   )
   return progressInstance
