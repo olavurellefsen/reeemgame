ac9b2140a9f554530d06f1df2fe116f400f6d0e2
Olavur Ellefsen <oe@tokni.com>
2019-04-30 09:42:03 +0100

    Convert score page to Material UI

--- src/MainContainer/ContentContainer/Score/Score.style.js
+++ src/MainContainer/ContentContainer/Score/Score.style.js
@@ -1,37 +1,19 @@
 import styled from 'styled-components'
+import Grid from '@material-ui/core/Grid'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
 
-export const Container = styled.div`
-  display: flex;
-  flex: 1;
-  justify-content: center;
-`
-export const Table = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 80%;
-  padding: 1%;
-`
+export const StyledGrid = styled(Grid)``
 
-export const Row = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  width: 100%;
-  padding-left: 15px;
-  padding-right: 15px;
-  flex-grow: 0;
-  background-color: ${props => (props.index % 2 ? 'white' : '#EDEDED')};
-`
-export const Cell = styled.div`
-  flex-grow: 1;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  width: 20%;
-`
-export const TextContainer = styled.div`
-  display: flex;
-  @import url('https://fonts.googleapis.com/css?family=Montserrat:500');
-  font-family: 'Montserrat', sans-serif;
-  margin: 10px;
-`
+export const StyledTable = styled(Table)``
+
+export const StyledTableBody = styled(TableBody)``
+
+export const StyledTableCell = styled(TableCell)``
+
+export const StyledTableHead = styled(TableHead)``
+
+export const StyledTableRow = styled(TableRow)``
