b63e30753a6fab1b7f66fa200c72e46db7d79d7b
Marita <maritafholm@gmail.com>
2019-05-28 12:51:24 +0200

    added dummy chart

--- src/MainContainer/ContentContainer/MainContent/MapContainer/CountryPopup.js
+++ src/MainContainer/ContentContainer/MainContent/MapContainer/CountryPopup.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { PropTypes } from 'prop-types'
+import Chart from 'react-google-charts'
+import sampleData from '../../../../data/sampledata.json'
 import {
   Container,
   Country,
@@ -11,6 +13,26 @@ import {
 import { Close } from '@material-ui/icons'
 
 export const CountryPopup = props => {
+  function getData(myCountry, scenario, currentYear, parameter) {
+    let data = [['Element', 'Density', { role: 'style' }]]
+    const countryData = sampleData.filter(
+      country =>
+        country.Country === myCountry &&
+        country.Parameter === parameter &&
+        country.Scenario === scenario
+    )
+    for (var i = 2015; i <= currentYear; i = i + 5) {
+      let year = [i, countryData[0][i], 'gray']
+      data.push(year)
+    }
+    // map(country => ({
+    //   code: country.Country.toLowerCase(),
+    //   color: convertToColor(country[currentYear], 90, 400),
+    //   value: country[currentYear],
+    //   unit: country.Unit,
+    // }))
+    return data
+  }
   return (
     <Container>
       <Header>
@@ -21,7 +43,14 @@ export const CountryPopup = props => {
           </Icon>
         </Button>
       </Header>
-      <Content />
+      <Content>
+        <Chart
+          chartType="ColumnChart"
+          width="100%"
+          heigth="400px"
+          data={getData('AT', 'C0T0E0', 2040, 'SpecifiedAnnual Demand')}
+        />
+      </Content>
     </Container>
   )
 }
