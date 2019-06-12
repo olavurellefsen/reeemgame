23a65903a233b2039351ef5d8b3ee529f2fef715
Marita <maritafholm@gmail.com>
2019-06-12 13:56:14 +0100

    added package

--- src/MainContainer/ContentContainer/MainContent/Share.js
+++ src/MainContainer/ContentContainer/MainContent/Share.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { useTranslation } from 'react-i18next'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 import Context from './../../../Context/Context'
 import {
   Container,
