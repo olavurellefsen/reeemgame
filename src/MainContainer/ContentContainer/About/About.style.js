725d2e95c209bda7273c5343e9f1afbc59b87853
Olavur Ellefsen <oe@tokni.com>
2019-07-08 12:45:39 +0100

    Update font family and size for about page

--- src/MainContainer/ContentContainer/About/About.style.js
+++ src/MainContainer/ContentContainer/About/About.style.js
@@ -2,4 +2,7 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
+  font-family: 'Roboto', sans-serif;
+  padding-bottom: 20px;
+  line-height: 1.6;
 `
