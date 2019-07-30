d8b5dcf63f4ff32a0724021ac1985ccfb1cb5d74
Marita <maritafholm@gmail.com>
2019-07-30 11:17:36 +0100

    created submenu for indicators

---
+++ src/MainContainer/ContentContainer/MainContent/IndicatorContainer/Indicator.style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+export const SubMenu = styled.div`
+  display: flex;
+  margin-left: 15px;
+`
