2cd11586911ca8ae9ba922fe50be99c36f0b7010
HauHe <haukeh@kth.se>
2021-08-19 16:26:43 +0200

    Completed functions to generate a dictionary with scenario names and paths to results.

--- src/utils/read_res.py
+++ src/utils/read_res.py
@@ -1,6 +1,38 @@
 "Script to determine the path of the scenario results of OSeMBE for the REEEMgame."
 #%% Import of needed packages
 import os
-#%% Get scenario names and paths
-dirs = [d for d in os.listdir('results') if os.path.isdir(os.path.join('results', d))]
+import pandas as pd
+#%% Get directory names from folder
+def get_dirs(path):
+    dirs = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
+    return dirs
+#%% Function to go through directories and build scenario names and paths
+def build_names(dic,i):
+    if i == 0:
+        p = dic[i]
+        dirs = get_dirs(p)
+        dic = {}
+        for f in dirs:
+            dic[f] = os.path.join(p,f)
+        i+=1
+    else:
+        dic_t = {}
+        for s in dic:
+            dirs = get_dirs(dic[s])
+            if 'res' in dirs:
+                i+=1
+                dic[s] = os.path.join(dic[s],'res')
+            else:
+                for d in dirs:
+                    dic_t[s+'|'+d] = os.path.join(dic[s],d)
+        if bool(dic_t):
+            dic = dic_t
+    return dic,i
+# %%
+dic_scen = {}
+i = 0
+dic_scen = {0: 'results'}
+while i < 2:
+    dic_scen, i = build_names(dic_scen,i)
+
 # %%
