50f37bb2d6197bb0ed30eb50b27b525d21197a73
HauHe <haukeh@kth.se>
2022-06-27 16:54:43 +0200

    Completed function to nomralise scores. Noticed that CO2 intensity needs inversion. Question is also if the intensity for one year is meaningfule.

--- src/utils/write_kpis.py
+++ src/utils/write_kpis.py
@@ -4,28 +4,43 @@ import pandas as pd
 from typing import Dict
 
 # for development
-#kpis = {}
-#kpis['CO2Intensity'] = pd.read_csv('results/220506/results/CO2Intensity_10th.csv')
-#kpis['DiscountedInvestmentPerCitizen'] = pd.read_csv('results/220506/results/DiscountedInvestmentPerCitizen_10th.csv')
-#kpis['LCOE'] = pd.read_csv('results/220506/results/LCOE_10th.csv')
+kpis = {}
+kpis['CO2Intensity'] = pd.read_csv('results/220506/results/CO2Intensity_10th.csv')
+kpis['DiscountedInvestmentPerCitizen'] = pd.read_csv('results/220506/results/DiscountedInvestmentPerCitizen_10th.csv')
+kpis['LCOE'] = pd.read_csv('results/220506/results/LCOE_10th.csv')
 
 def filter_kpis(data: pd.DataFrame) -> pd.DataFrame:
-    data = data[data['YEAR']==2050] # change to 2050!!!
+    data = data[data['YEAR']==2050]
     data = data.drop(['YEAR'], axis=1)
     return data.set_index('REGION')
+
+def normalise_scores(df: pd.DataFrame) -> pd.DataFrame:
+    
+    df_norm_score = pd.DataFrame(columns=['REGION', 'SCENARIO','VALUE'])
+    max_value = df.iloc[0].max()
+    score_array = df.iloc[0].to_numpy()
+
+    score_norm = (score_array / max_value) * 100
+
+    df_norm_score['SCENARIO'] = list(df)
+    df_norm_score['VALUE'] = score_norm
+    df_norm_score['REGION'] = df.index[0]
+
+    return df_norm_score
 
 def main(kpis: Dict):
 
     for kpi in kpis:
         kpis[kpi] = filter_kpis(kpis[kpi])
+        kpis[kpi] = normalise_scores(kpis[kpi])
 
     rawScores = []
 
-    for s in kpis['CO2Intensity'].columns:
-        for c in kpis['CO2Intensity'].index:
-            env = kpis['CO2Intensity'].loc[c, s]
-            eco = kpis['DiscountedInvestmentPerCitizen'].loc[c, s]
-            soc = kpis['LCOE'].loc[c, s]
+    for s in kpis['CO2Intensity']['SCENARIO'].unique():
+        for c in kpis['CO2Intensity']['REGION'].unique():
+            env = kpis['CO2Intensity'][(kpis['CO2Intensity']['REGION']==c)&(kpis['CO2Intensity']['SCENARIO']==s)].iloc[0,2]
+            eco = kpis['DiscountedInvestmentPerCitizen'][(kpis['DiscountedInvestmentPerCitizen']['REGION']==c)&(kpis['DiscountedInvestmentPerCitizen']['SCENARIO']==s)].iloc[0,2]
+            soc = kpis['LCOE'][(kpis['LCOE']['REGION']==c)&(kpis['LCOE']['SCENARIO']==s)].iloc[0,2]
             rawScores.append(
                 {
                     "scenario": s,
