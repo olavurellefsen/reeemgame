b0cef11016e824f694b3b3312693da408653e7ea
HauHe <haukeh@kth.se>
2023-04-18 12:19:53 +0200

    Made German translation accessible in game.

--- src/MainContainer/ContentContainer/About/About.js
+++ src/MainContainer/ContentContainer/About/About.js
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next'
 import { Container } from './About.style'
 import { Page } from '../../PageRenderer'
 import about_en from '../../../Markdown/about.en.md'
+import about_de from '../../../Markdown/about.de.md'
 import about_dk from '../../../Markdown/about.dk.md'
 import about_fo from '../../../Markdown/about.fo.md'
 
@@ -14,6 +15,9 @@ export const About = () => {
     case 'en':
       file = about_en
       break
+      case 'de':
+        file = about_de
+        break
     case 'dk':
       file = about_dk
       break
