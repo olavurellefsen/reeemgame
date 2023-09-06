1ee6f2d50e126e7ba82f6b89352278e3ac8e774f
HauHe <haukeh@kth.se>
2023-09-06 15:47:10 +0200

    Changed score calculation, so that each POV has a best (100) and a worst (0) scenario.

--- src/utils/CalculateScore.js
+++ src/utils/CalculateScore.js
@@ -9,8 +9,8 @@ export function calculateScore(selectedScenario, weights) {
     if (scenario) {
       let { env, eco, soc } = scenario
       normalizedScore = Math.round(
-        (100 * (env * weights.env + eco * weights.eco + soc * weights.soc)) /
-        findMaxScore(weights)
+        (((env * weights.env + eco * weights.eco + soc * weights.soc)-findMinScore(weights)) /
+        (findMaxScore(weights) - findMinScore(weights))) * 100
       )
     } else {
       console.log(`No scenario found for ${selectedScenario}`)
@@ -50,4 +50,36 @@ const findMaxScore = weights => {
   if (maxScore === 0) maxScore = -1
   //console.log('best scenario: ', bestScenario)
   return maxScore
+}
+
+const findMinScore = weights => {
+  let minScore = 100
+  //let bestScenario = 'none'
+  if (Object.entries(weights).length !== 0 && weights.constructor === Object) {
+    //For each possible scenario
+    for (var t = 0; t <= 1; t++) {
+      for (var e = 0; e <= 26; e++) {
+        for (var c = 0; c <= 7; c++) {
+          for (var b = 0; b <= 3; b++) {
+            const checkScenario = 'T' + t + 'E' + e + 'C' + c + 'B' + b
+            let scenario = scenarioScore.filter(
+              scenario => scenario.scenario === checkScenario
+            )[0]
+            if (scenario) {
+              let { env, eco, soc } = scenario
+              let score = env * weights.env + eco * weights.eco + soc * weights.soc
+              if (score < minScore) {
+                minScore = score
+              }
+            } else {
+              console.log(`No scenario found for ${checkScenario}`)
+            }
+          }
+        }
+      }
+    }
+  }
+  if (minScore === 100) minScore = -1
+  //console.log('best scenario: ', bestScenario)
+  return minScore
 }
